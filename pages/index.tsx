0f23daf43c5b4220b85abc622d8d57a87381d0b0
Robinson Lara <rlara@cfchildren.org>
2022-07-21 16:10:09 -0400

    static only

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,9 @@ const Home: NextPage = () => {
     return (
         <>
             <p>Hello: Go and see the pages in:</p>
-            <Link href="/ssr">
-                <a>Server Side Rendered Content</a>
-            </Link>
+            {/*<Link href="/ssr">*/}
+            {/*    <a>Server Side Rendered Content</a>*/}
+            {/*</Link>*/}
             <br/>
             <Link href="/static">
                 <a>Static Generated Content</a>
